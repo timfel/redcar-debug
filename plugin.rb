11acc72327b4def352a4ed52c565b2528c6634bd
Tim Felgentreff <timfelgentreff@gmail.com>
2010-08-22 18:37:08 +0200

    remove dependency on runnables and bump version

--- plugin.rb
+++ plugin.rb
@@ -1,11 +1,10 @@
 Plugin.define do
   name "gdi"
-  version "0.0.1"
+  version "0.0.2"
 
   object "Redcar::GDI"
   file "lib", "gdi"
 
   dependencies  "core",      ">0",
-                "HTML View", ">=0.3.2",
-                "runnables", ">=1.0"
+                "HTML View", ">=0.3.2"
 end
