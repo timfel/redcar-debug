d388d6fe17cb1cf3dd262b9dade7bd4879ead46f
Tim Felgentreff <timfelgentreff@gmail.com>
2010-09-07 19:08:40 +0200

    implement file matching for jdb

--- lib/gdi/debugger/jdb.rb
+++ lib/gdi/debugger/jdb.rb
@@ -30,6 +30,32 @@ class Redcar::GDI::Debugger::JDB < Redcar::GDI::Debugger
     @breakpoint_hit = (stdout =~ /\[[0-9]+\] $/)
   end
 
+  # FIXME: Don't just use Dir.glob, open the FindFileDialog, if necessary
+  def add_file_link(matched_text, file_part, lineno_part)
+    Redcar::GDI::FileLink.new.tap do |l|
+      l.match = matched_text
+      if lineno_part
+        # Line breakpoint
+        file_part = "#{File.join(file_part.split("."))}.java"
+        l.line = lineno_part
+      else
+        # Method breakpoint, exclude method
+        parts = file_part.split(".")
+        file_part = "#{File.join(parts[0...-1])}.java"
+        l.line = 1
+        method= parts.last # TODO: Jump to method, not to top
+      end
+      l.file = Dir.glob(File.join(Project::Manager.focussed_project.path, "**", file_part)).first
+    end
+  end
+
+  def file_pattern
+    path_segment  = /[A-Za-z\$0-9]+/
+    path_segments = /(?:#{path_segment}\.)+/
+    line_number   = /[1-9][0-9]*/
+    /(#{path_segments}#{path_segment})(?::(#{line_number}))?/
+  end
+
   def src_extension
     /\.java/
   end
