46180df47d97b1804fb6546893dd8d473c6e098e
Tim Felgentreff <timfelgentreff@gmail.com>
2010-09-26 12:41:47 +0200

    use different class nesting for sane constant lookup

--- lib/gdi/debugger/hijack.rb
+++ lib/gdi/debugger/hijack.rb
@@ -4,20 +4,22 @@ require 'gdi/debugger/rdebug'
 # It differs from plain RDebug only in that it first
 # displays an IRB prompt which is not for debugging,
 # but for setting breakpoints
-class GDI::Debugger
-  class Hijack < RDebug
-    Commandline = "hijack "
+class GDI
+  class Debugger
+    class Hijack < RDebug
+      Commandline = "hijack "
 
-    display_name "Hijack for MRI"
+      display_name "Hijack for MRI"
 
-    def query(info)
-      # If we're at an irb prompt, we cannot run our info queries
-      super unless @irb_prompt
-    end
+      def query(info)
+        # If we're at an irb prompt, we cannot run our info queries
+        super unless @irb_prompt
+      end
 
-    # Overwrite default behaviour to get instance state behaviour
-    def prompt_ready?(stdout)
-      (@irb_prompt =~ />> $/) || super
+      # Overwrite default behaviour to get instance state behaviour
+      def prompt_ready?(stdout)
+        (@irb_prompt =~ />> $/) || super
+      end
     end
   end
 end
--- lib/gdi/debugger/rdebug.rb
+++ lib/gdi/debugger/rdebug.rb
@@ -1,38 +1,40 @@
-class GDI::Debugger
-  class RDebug < Base
-    Commandline = "rdebug -c "
-    Backtrace = "backtrace"
-    Threads = "info threads"
-    Breakpoints = "info breakpoints"
-    Variables = "info variables"
-    Evaluate = "p"
-    Break = "break"
+class GDI
+  class Debugger
+    class RDebug < Base
+      Commandline = "rdebug -c "
+      Backtrace = "backtrace"
+      Threads = "info threads"
+      Breakpoints = "info breakpoints"
+      Variables = "info variables"
+      Evaluate = "p"
+      Break = "break"
 
-    auto_connecting!
-    display_name "Ruby Debug"
-    prompt_ready? {|stdout| (stdout =~ /\(rdb:[A-Za-z0-9]+\)\s+$/) }
-    src_extensions /\.(?:rb|rhtml|html\.erb)/
-    html_elements({:partial => "repl"},
-      {:partial => "window", :name => "Backtrace", :id => "backtrace"},
-      {:partial => "notebook", :windows => [
-        {:name => "Variables", :id => "variables"},
-        {:name => "Threads", :id => "threads"},
-        {:name => "Breakpoints", :id => "breakpoints"} ] })
+      auto_connecting!
+      display_name "Ruby Debug"
+      prompt_ready? {|stdout| (stdout =~ /\(rdb:[A-Za-z0-9]+\)\s+$/) }
+      src_extensions /\.(?:rb|rhtml|html\.erb)/
+      html_elements({:partial => "repl"},
+        {:partial => "window", :name => "Backtrace", :id => "backtrace"},
+        {:partial => "notebook", :windows => [
+          {:name => "Variables", :id => "variables"},
+          {:name => "Threads", :id => "threads"},
+          {:name => "Breakpoints", :id => "breakpoints"} ] })
 
-    def initialize(output, process)
-      super
-      automatic_queries
-    end
-
-    def automatic_queries
-      @process.add_listener(:prompt_ready) do
-        [:Backtrace, :Threads, :Breakpoints, :Variables].each {|info| query(info) }
+      def initialize(output, process)
+        super
+        automatic_queries
       end
-    end
 
-    def query(info)
-      output = wait_for(self.class.const_get(info)) {|stdout| prompt_ready? stdout }
-      @output.replace(output, info.to_s.downcase)
+      def automatic_queries
+        @process.add_listener(:prompt_ready) do
+          [:Backtrace, :Threads, :Breakpoints, :Variables].each {|info| query(info) }
+        end
+      end
+
+      def query(info)
+        output = wait_for(self.class.const_get(info)) {|stdout| prompt_ready? stdout }
+        @output.replace(output, info.to_s.downcase)
+      end
     end
   end
 end
