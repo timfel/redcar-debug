60d7fcbcc8538a1508139b5994f931a3e64ee9e1
Tim Felgentreff <timfelgentreff@gmail.com>
2010-09-26 14:50:29 +0200

    different lookup, again

--- lib/gdi/debugger/base.rb
+++ lib/gdi/debugger/base.rb
@@ -1,16 +1,20 @@
-class GDI::Debugger::Base < GDI::Debugger
-  abstract!
-  file_linker GDI::Debugger::Files::DefaultLinker
-  display_name :name
-  before_output :process_links
+class GDI
+  class Debugger
+    class Base < Debugger
+      abstract!
+      file_linker Files::DefaultLinker
+      display_name :name
+      before_output :process_links
 
-  def process_links(text)
-    text = @file_linker.find_links(text).inject(text) do |txt, l|
-      txt.sub(l[:match], <<-HTML)
-      <span class="file_link" data-file="#{l[:file]}" data-line="#{l[:line]}">
-      #{l[:match]}
-      </span>
-      HTML
+      def process_links(text)
+        text = @file_linker.find_links(text).inject(text) do |txt, l|
+          txt.sub(l[:match], <<-HTML)
+            <span class="file_link" data-file="#{l[:file]}" data-line="#{l[:line]}">
+              #{l[:match]}
+            </span>
+          HTML
+        end
+      end
     end
   end
 end
