5954c64392010f1738935bccdeb534992d4d3088
Tim Felgentreff <timfelgentreff@gmail.com>
2010-09-26 13:11:41 +0200

    last nesting change

--- lib/gdi/debugger/gdb.rb
+++ lib/gdi/debugger/gdb.rb
@@ -1,33 +1,35 @@
-class GDI::Debugger
-  class Gdb < Base
-    Commandline = "gdb -p "
-    Backtrace = "bt"
-    Locals = "info locals"
-    Registers = "info reg"
-    Breakpoints = "info breakpoints"
-    Evaluate = "print"
-    Break = "break"
+class GDI
+  class Debugger
+    class Gdb < Base
+      Commandline = "gdb -p "
+      Backtrace = "bt"
+      Locals = "info locals"
+      Registers = "info reg"
+      Breakpoints = "info breakpoints"
+      Evaluate = "print"
+      Break = "break"
 
-    display_name "GNU Debugger"
-    prompt_ready? {|stdout| stdout.end_with? "(gdb) " }
-    src_extensions /\.(?:cpp|c)/
-    html_elements({:partial => "repl"},
-      {:partial => "window", :name => "Backtrace", :id => "backtrace"},
-      {:partial => "notebook", :windows => [
-        {:name => "Locals", :id => "locals"},
-        {:name => "Registers", :id => "registers"},
-        {:name => "Breakpoints", :id => "breakpoints"} ] })
+      display_name "GNU Debugger"
+      prompt_ready? {|stdout| stdout.end_with? "(gdb) " }
+      src_extensions /\.(?:cpp|c)/
+      html_elements({:partial => "repl"},
+        {:partial => "window", :name => "Backtrace", :id => "backtrace"},
+        {:partial => "notebook", :windows => [
+          {:name => "Locals", :id => "locals"},
+          {:name => "Registers", :id => "registers"},
+          {:name => "Breakpoints", :id => "breakpoints"} ] })
 
-    def initialize(output, process)
-      super
-      automatic_queries
-    end
+      def initialize(output, process)
+        super
+        automatic_queries
+      end
 
-    def automatic_queries
-      process.add_listener(:prompt_ready) do
-        [:Backtrace, :Locals, :Breakpoints, :Registers].each do |query|
-          out = wait_for(self.class.const_get(query)) {|stdout| prompt_ready? stdout }
-          output.replace(out, query.to_s.downcase)
+      def automatic_queries
+        process.add_listener(:prompt_ready) do
+          [:Backtrace, :Locals, :Breakpoints, :Registers].each do |query|
+            out = wait_for(self.class.const_get(query)) {|stdout| prompt_ready? stdout }
+            output.replace(out, query.to_s.downcase)
+          end
         end
       end
     end
